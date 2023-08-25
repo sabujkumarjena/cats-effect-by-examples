0be7084d886e85c5cf1820c4a72f0a1298b64ec3
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-25 12:00:58 +0530

    Resources

--- src/main/scala/catsEffectByExample/section3/Resources.scala
+++ src/main/scala/catsEffectByExample/section3/Resources.scala
@@ -1,16 +1,20 @@
 package catsEffectByExample.section3
 
-import cats.effect.{IO, IOApp}
+import cats.effect.kernel.Outcome.{Canceled, Errored, Succeeded}
+import cats.effect.{IO, IOApp, Resource}
 
 import java.io.{File, FileReader}
 import java.util.Scanner
 import scala.concurrent.duration.*
 
 object Resources extends IOApp.Simple {
+
   import catsEffectByExample.utils.*
+
   // use-case: manage a connection lifecycle
   class Connection(url: String) {
     def open(): IO[String] = IO(s"Opening connection to $url").debug
+
     def close(): IO[String] = IO(s"Closing connection to $url").debug
   }
 
@@ -54,6 +58,7 @@ object Resources extends IOApp.Simple {
   def openFileScanner(path: String): IO[Scanner] = IO(
     new Scanner(new FileReader(new File(path)))
   )
+
   def readLineByLine(scanner: Scanner): IO[Unit] =
     if (scanner.hasNextLine)
       IO(scanner.nextLine()).debug >> IO.sleep(100.millis) >> readLineByLine(
@@ -78,13 +83,117 @@ object Resources extends IOApp.Simple {
         IO(s"Closing the file at $path").debug >> IO(scanner.close()).void
       )
 
+  /** Resources
+    */
+  def connFromConfig(path: String): IO[Unit] =
+    openFileScanner(path)
+      .bracket { scanner =>
+        // acuire a connection
+        IO(new Connection(scanner.nextLine())).bracket { conn =>
+          conn.open().debug >> IO.never
+        }(conn => conn.close().void)
+      }(scanner => IO("closing file").debug >> IO(scanner.close()))
+  // nesting resources are tedious
+
+  val connectionResource =
+    Resource.make(IO(new Connection("google.com")))(conn => conn.close().void)
+
+  // ... at a later part of your code
+  val resourceFetchUrl = for {
+    fib <- connectionResource.use(conn => conn.open() >> IO.never).start
+    _ <- IO.sleep(5.seconds) >> fib.cancel
+  } yield ()
+
+  // resources are equivalent to brackets
+  val simpleResource = IO("some resource")
+  val usingResource: String => IO[String] = string =>
+    IO(s"using the string: $string").debug
+  val releaseResource: String => IO[Unit] = string =>
+    IO(s"finalising the string: $string").debug.void
+
+  val usingResourceWithBracket =
+    simpleResource.bracket(usingResource)(releaseResource)
+  val usingResourceWithResource =
+    Resource.make(simpleResource)(releaseResource).use(usingResource)
+
+  /** Exercise : read a test file with one line every 100 millis, using Resource
+    */
+  def resourceReadFile(
+      path: String
+  ): IO[Unit] = {
+    val scannerResource: IO[Scanner] = openFileScanner(path)
+    val releaseScanner: Scanner => IO[Unit] = scanner =>
+      IO(s"Closing the file at $path").debug >> IO(scanner.close()).void
+    val useScanner: Scanner => IO[Unit] = scanner => readLineByLine(scanner)
+    Resource.make(scannerResource)(releaseScanner).use(useScanner)
+  }
+
+  def cancelReadFile(path: String) = for {
+    fib <- resourceReadFile(path).start
+    _ <- IO.sleep(10.seconds) >> fib.cancel
+  } yield ()
+
+  // nested resources
+  def connFromConfResource(path: String) =
+    Resource
+      .make(IO("Openining file").debug >> openFileScanner(path))(scanner =>
+        IO("closing file").debug >> IO(scanner.close())
+      )
+      .flatMap(scanner =>
+        Resource.make(IO(new Connection(scanner.nextLine())))(conn =>
+          conn.close().void
+        )
+      )
+
+  def connFromConfResourceClean(path: String) = for {
+    scanner <- Resource
+      .make(IO("Openining file").debug >> openFileScanner(path))(scanner =>
+        IO("closing file").debug >> IO(scanner.close())
+      )
+    conn <-
+      Resource.make(IO(new Connection(scanner.nextLine())))(conn =>
+        conn.close().void
+      )
+  } yield conn
+
+  val openConnection = connFromConfResourceClean(
+    "/Users/sabuj/Downloads/cats-effect-by-examples/src/main/scala/catsEffectByExample/section3/Resources.scala"
+  ).use(conn => conn.open() >> IO.never)
+
+  val canceledConnection = for {
+    fib <- openConnection.start
+    _ <- IO.sleep(3.seconds) >> IO("cancelling").debug >> fib.cancel
+  } yield ()
+
+  // finalizer to regular IOs
+  val ioWithFinalizer =
+    IO("some resource").debug.guarantee(IO("freeing resource").debug.void)
+
+  val ioWithFinalizer_v2 = IO("some resources").debug.guaranteeCase {
+    case Succeeded(fa) => fa.flatMap((result => IO(s"releasing resource: $result").debug)).void
+    case Errored(e) => IO("nothing to release").debug.void
+    case Canceled() => IO("resource got canceled, releasing what's left").debug.void
+  }
   override def run: IO[Unit] = {
+
     // asyncFetchUrl.void
     // correctAsyncFetchUrl.void
     // bracketProgram
-    bracketReadFile_v2(
-      "/Users/sabuj/Downloads/cats-effect-by-examples/src/main/scala/catsEffectByExample/section3/Resources.scala"
-    )
+//    bracketReadFile_v2(
+//      "/Users/sabuj/Downloads/cats-effect-by-examples/src/main/scala/catsEffectByExample/section3/Resources.scala"
+//    )
+
+    // resourceFetchUrl
+
+    // resourceReadFile( "/Users/sabuj/Downloads/cats-effect-by-examples/src/main/scala/catsEffectByExample/section3/Resources.scala")
+//    cancelReadFile(
+//      "/Users/sabuj/Downloads/cats-effect-by-examples/src/main/scala/catsEffectByExample/section3/Resources.scala"
+//    )
+
+    // openConnection.void
+
+   // canceledConnection
+   ioWithFinalizer.void
   }
 
 }
