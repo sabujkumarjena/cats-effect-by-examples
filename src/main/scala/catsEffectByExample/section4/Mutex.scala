2dac684f92c2c0269f2d43de961d33be721eb12f
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-30 13:51:45 +0530

    mutex fixed

--- src/main/scala/catsEffectByExample/section4/Mutex.scala
+++ src/main/scala/catsEffectByExample/section4/Mutex.scala
@@ -1,12 +1,16 @@
 package catsEffectByExample.section4
 
 import cats.effect.kernel.Outcome.{Canceled, Errored, Succeeded}
-import cats.effect.{Deferred, IO, IOApp, Ref}
+import cats.effect.{Concurrent, Deferred, IO, IOApp, Ref}
 
 import scala.util.Random
 import scala.concurrent.duration.*
 import catsEffectByExample.utils.*
 import cats.syntax.parallel.*
+import cats.syntax.functor.*
+import cats.syntax.flatMap.*
+import cats.effect.syntax.monadCancel.*
+import catsEffectByExample.section4.Mutex.MutexV2
 
 import scala.collection.immutable.Queue
 
@@ -27,7 +31,9 @@ object Mutex {
       createSignal().flatMap { signal =>
         val cleanup = state.modify { case State(locked, queue) =>
           val newQueue = queue.filterNot(_ eq signal)
-          State(locked, newQueue) -> release
+          val isBlocking = queue.exists(_ eq signal)
+          val decision = if (isBlocking) IO.unit else release
+          State(locked, newQueue) -> decision // problem
         }.flatten
         state.modify {
           case State(false, _) =>
@@ -49,6 +55,67 @@ object Mutex {
           State(locked = true, rest) -> signal.complete(()).void
         }
     }.flatten
+  }
+
+  // generic mutex after the polymorphic concurrent exercise
+  abstract class MutexV2[F[_]] {
+    def acquire: F[Unit]
+
+    def release: F[Unit]
+  }
+
+  object MutexV2 {
+    type Signal[F[_]] = Deferred[F, Unit]
+
+    case class State[F[_]](locked: Boolean, waiting: Queue[Signal[F]])
+
+    def unlocked[F[_]] = State[F](locked = false, Queue())
+
+    def createSignal[F[_]](using concurrent: Concurrent[F]): F[Signal[F]] =
+      concurrent.deferred[Unit]
+
+    def create[F[_]](using concurrent: Concurrent[F]): F[MutexV2[F]] =
+      concurrent
+        .ref(unlocked)
+        .map(initialState => createMutexWithCancellation(initialState))
+
+    def createMutexWithCancellation[F[_]](
+        state: Ref[F, State[F]]
+    )(using concurrent: Concurrent[F]): MutexV2[F] =
+      new MutexV2[F] {
+        override def acquire = concurrent.uncancelable { poll =>
+          createSignal.flatMap { signal =>
+
+            val cleanup = state.modify { case State(locked, queue) =>
+              val newQueue = queue.filterNot(_ eq signal)
+              val isBlocking = queue.exists(_ eq signal)
+              val decision = if (isBlocking) concurrent.unit else release
+              State(locked, newQueue) -> decision // problem
+
+            }.flatten
+
+            state.modify {
+              case State(false, _) =>
+                State[F](locked = true, Queue()) -> concurrent.unit
+              case State(true, queue) =>
+                State[F](locked = true, queue.enqueue(signal)) -> poll(
+                  signal.get
+                ).onCancel(cleanup)
+            }.flatten
+          }
+        }
+
+        override def release = state.modify {
+          case State(false, _) => unlocked[F] -> concurrent.unit
+          case State(true, queue) =>
+            if (queue.isEmpty) unlocked[F] -> concurrent.unit
+            else {
+              val (signal, rest) = queue.dequeue
+              State[F](locked = true, rest) -> signal.complete(()).void
+            }
+        }.flatten
+      }
+
   }
 
   def createSimpleMutex(state: Ref[IO, State]): Mutex = new Mutex {
@@ -110,6 +177,19 @@ object MutexPlayGround extends IOApp.Simple {
     _ <- IO(s"[task $id] lock removed...").debug
   } yield res
 
+  def createLockingTask_v2(id: Int, mutex: MutexV2[IO]): IO[Int] = for {
+    _ <- IO(s"[task $id] waiting for permission...").debug
+    _ <-
+      mutex.acquire // blocks if the mutex has been acquired by some other fiber
+      // critical section
+    _ <- IO(s"[task $id] working...").debug
+    res <- criticalTask()
+    _ <- IO(s"[task $id] got result: $res").debug
+    // critical section end
+    _ <- mutex.release
+    _ <- IO(s"[task $id] lock removed...").debug
+  } yield res
+
   def demoLockingTasks() = for {
     mutex <- Mutex.create
     results <- (1 to 10).toList.parTraverse(id => createLockingTask(id, mutex))
@@ -132,6 +212,23 @@ object MutexPlayGround extends IOApp.Simple {
       } yield result
   }
 
+  def createCancellingTask_v2(id: Int, mutex: MutexV2[IO]): IO[Int] = {
+    if (id % 2 == 0) createLockingTask_v2(id, mutex)
+    else
+      for {
+        fib <- createLockingTask_v2(id, mutex)
+          .onCancel(IO(s"[task $id] received cancellation").debug.void)
+          .start
+        _ <- IO.sleep(2.seconds) >> fib.cancel
+        out <- fib.join
+        result <- out match {
+          case Succeeded(effect) => effect
+          case Errored(_)        => IO(-1)
+          case Canceled()        => IO(-2)
+        }
+      } yield result
+  }
+
   def demoCancellingTasks() = for {
     mutex <- Mutex.create
     results <- (1 to 10).toList.parTraverse(id =>
@@ -139,9 +236,43 @@ object MutexPlayGround extends IOApp.Simple {
     )
   } yield results
 
+  def demoCancellingTasks_v2() = for {
+    mutex <- MutexV2.create[IO]
+    results <- (1 to 10).toList.parTraverse(id =>
+      createCancellingTask_v2(id, mutex)
+    )
+  } yield results
+
+  def demoCancelWhileBlock() = for {
+    mutex <- MutexV2.create[IO]
+    fib1 <- (IO("[fib1] getting mutex").debug >> mutex.acquire >> IO(
+      "[fib1] got the mutex, never releasing"
+    ).debug >> IO.never).start
+
+    fib2 <- (IO("[fib2] sleeping").debug >> IO.sleep(3.seconds) >> IO(
+      " Trying to get mutex"
+    ).debug >> mutex.acquire.onCancel(
+      IO("[fib2] being cancelled").debug.void
+    ) >> IO("[fib2] acquired mutex").debug).start
+
+    fib3 <- (IO("[fib3] sleeping").debug >> IO.sleep(2500.millis) >> IO(
+      " Trying to get mutex"
+    ).debug >> mutex.acquire >> IO(
+      "[fib3] acquired mutex:: if this shows, then FAIL"
+    ).debug).start
+
+    _ <- IO.sleep(4.seconds) >> IO("CANCELLING fib 2! ").debug >> fib2.cancel
+    _ <- fib1.join
+    _ <- fib2.join
+    _ <- fib3.join
+  } yield ()
+
   override def run: IO[Unit] = {
     // demoNonLockingTasks().debug.void
-   // demoLockingTasks().debug.void
-   demoCancellingTasks().debug.void
+    // demoLockingTasks().debug.void
+    // demoCancellingTasks().debug.void
+    // demoCancellingTasks_v2().debug.void
+
+    demoCancelWhileBlock().debug.void
   }
 }
